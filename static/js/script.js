9997bd41784afa99e134db5c4fb8e057d16a7eee
Alexandre-Michael <alexandremi2024@gmail.com>
2024-08-10 21:00:57 -0300

    💄 Updated menu color and file paths

--- static/js/script.js
+++ static/js/script.js
@@ -2,9 +2,9 @@ function menuShow() {
     let menuMobile = document.querySelector('.mobile-menu');
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
-        document.querySelector('.icon').src = "static/images/black-menu.png";
+        document.querySelector('.icon').src = "static/images/purple-menu.png";
     } else {
         menuMobile.classList.add('open');
-        document.querySelector('.icon').src = "static/images/black-close-menu.png";
+        document.querySelector('.icon').src = "static/images/purple-close-menu.png";
     }
 }
